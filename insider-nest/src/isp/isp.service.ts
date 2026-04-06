0d943d368311e5c6451781f67c330a103b39bd9a
raihan <devgbj2@gmail.com>
2026-04-06 09:15:58 +0700

    fix: api dir

--- insider-nest/src/isp/isp.service.ts
+++ insider-nest/src/isp/isp.service.ts
@@ -27,7 +27,7 @@ export class IspService {
         const skip = (page - 1) * limit;
 
         // Bangun where clause secara type-safe
-        const where: Prisma.InternetServiceProviderWhereInput = {
+        const where = {
             ...(name && {
                 name: { contains: name, mode: 'insensitive' },
             }),
